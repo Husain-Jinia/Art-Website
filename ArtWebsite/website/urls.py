9c76ed5140ad81ca43e26ce2523c30e576db0fe0
Husain Jinia <husainjinia0201@gmail.com>
2021-08-02 19:42:51 +0530

    Added Home page view and style to Art page

--- ArtWebsite/website/urls.py
+++ ArtWebsite/website/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import artPage
+from .views import artPage, home
 
 urlpatterns = [
-    path('', artPage, name="artpage")
+    path('', artPage, name="artpage"),
+    path('home', artPage, name="homepage")
 ]
--- ArtWebsite/website/views.py
+++ ArtWebsite/website/views.py
@@ -5,3 +5,6 @@ from .models import Art
 def artPage(request):
     art = Art.objects.all()
     return render(request,'artPage.html',{'arts':art})
+
+def home(request):
+    return render(request, 'homePage.html')
